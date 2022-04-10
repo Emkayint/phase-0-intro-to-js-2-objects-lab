6641c59f5726b1d8198283fb7a490cd5030bb0f1
kiyaiB <babra.kiyai@student.moringaschool.com>
2022-04-10 15:04:10 +0300

    Update Key and value

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {
+    name : "Sam",
+}
+
+function updateEmployeeWithKeyAndValue(employee,  keys, value){
+    let work = {...employee}
+    work[keys] = value;
+    return work
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee, "John", "Doe"))
